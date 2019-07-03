11fb2feea3bf1e3a2b7f811fb34a4dc1e5816057
adudley78 <dudleyadam@gmail.com>
2019-07-03 16:05:10 -0400

    Implement Redux DevTools

--- app/javascript/configureStore.js
+++ app/javascript/configureStore.js
@@ -4,28 +4,35 @@ import { createStore, applyMiddleware } from 'redux';
 
 import thunk from 'redux-thunk';
 
+import { composeWithDevTools } from 'redux-devtools-extension';
+
 const initialState = {
     things: [
-        {
-            name: "test",
-            guid: "123"
-        }
+        // {
+        //     name: "test",
+        //     guid: "123"
+        // }
     ]
 };
 
 function rootReducer(state, action) {
     console.log(action.type);
     switch (action.type) {
-        default:
-            return state
+        case "GET_THINGS_SUCCESS":
+            return { things: action.json.things };
     }
+    return state;
 }
 
 export default function configureStore() {
     const store = createStore(
         rootReducer,
         initialState,
-        applyMiddleware(thunk)
+        composeWithDevTools(
+            applyMiddleware(
+                thunk,
+            )
+        )
     );
     return store;
 }
