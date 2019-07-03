73d11645167b7393ff41b9cf246a301911fe931d
adudley78 <dudleyadam@gmail.com>
2019-07-03 11:07:10 -0400

    Build configureStore.js

---
+++ app/javascript/configureStore.js
@@ -0,0 +1,20 @@
+// like index.js
+
+import { createStore } from 'redux';
+
+const initialState = {
+    things: []
+};
+
+function rootReducer(state, action) {
+    console.log(action.type);
+    switch (action.type) {
+        default:
+            return state
+    }
+}
+
+export default function configureStore() {
+    const store = createStore(rootReducer, initialState);
+    return store;
+}
