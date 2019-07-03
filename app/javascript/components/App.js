6619b79dfb23293a06d0c695e36e083a4a64f01b
adudley78 <dudleyadam@gmail.com>
2019-07-03 11:08:53 -0400

    Set up Redux store in App.js

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -3,17 +3,21 @@ import PropTypes from "prop-types"
 
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 
+import { Provider } from 'react-redux';
+
 import HelloWorld from './HelloWorld'
 
 class App extends React.Component {
   render () {
     return (
-      <BrowserRouter>
-        <Switch>
-          <Route exact path="/" render={() => ("Home!")} />
-          <Route path="/hello" render={() => <HelloWorld greeting="Friend"/>} />
-        </Switch>
-      </BrowserRouter>
+      <Provider store={store}>
+        <BrowserRouter>
+          <Switch>
+            <Route exact path="/" render={() => ("Home!")} />
+            <Route path="/hello" render={() => <HelloWorld greeting="Friend"/>} />
+          </Switch>
+        </BrowserRouter>
+      </Provider>
     );
   }
 }
