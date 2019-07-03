9fb256b46e81fff20c5f5b4c1c08247963082a9e
adudley78 <dudleyadam@gmail.com>
2019-07-03 15:20:48 -0400

    Add action dispatch on button click

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -7,6 +7,9 @@ import { Provider } from 'react-redux';
 
 import HelloWorld from './HelloWorld'
 
+import configureStore from '../configureStore'
+const store = configureStore();
+
 class App extends React.Component {
   render () {
     return (
--- app/javascript/components/HelloWorld.js
+++ app/javascript/components/HelloWorld.js
@@ -1,18 +1,39 @@
 import React from "react"
-import PropTypes from "prop-types"
+// import PropTypes from "prop-types"
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
+
+const GET_THINGS_REQUEST = 'GET_THINGS_REQUEST';
+
+// refactor into actions folder
+function getThings() {
+  console.log('getThings() Action!!')
+  return {
+    type: GET_THINGS_REQUEST
+  };
+};
 
 class HelloWorld extends React.Component {
   render () {
     return (
       <React.Fragment>
+        
         Greeting: {this.props.greeting}
+        <button className="getThingsBtn" onClick={() => this.props.getThings()}>getThings</button>
+
       </React.Fragment>
     );
   }
 }
 
-HelloWorld.propTypes = {
-  greeting: PropTypes.string
-};
+const structuredSelector = createStructuredSelector({
+  things: state => state.things,
+});
 
-export default HelloWorld
+// HelloWorld.propTypes = {
+//   greeting: PropTypes.string
+// };
+
+const mapDispatchToProps = { getThings };
+
+export default connect(structuredSelector, mapDispatchToProps)(HelloWorld);
