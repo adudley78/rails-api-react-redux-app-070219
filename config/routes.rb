94589aab1b6d769339ff42a4e815fd031e838349
adudley78 <dudleyadam@gmail.com>
2019-07-03 11:04:23 -0400

    Add App.js and implement React Router

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
+  
   root 'static#index'
+
+  get '*page', to: 'static#index', constraints: ->(req) do
+    !req.xhr? && req.format.html?
+  end
+
   namespace :v1, defaults: { format: 'json' } do
     get 'things', to: 'things#index'
   end
+
 end
