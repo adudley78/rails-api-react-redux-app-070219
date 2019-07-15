f85639addc32f8d496bf9f1380936c1490d19317
adudley78 <dudleyadam@gmail.com>
2019-07-15 13:01:14 -0400

    Build Articles.js

---
+++ app/javascript/components/articles/Articles.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import Article from './Article';
+
+class Articles extends Component {
+
+    render() {
+        const { articles, deleteArticle } = this.props;
+        const articleList = articles.map(article => {
+            return (
+                <Article
+                    key={article.id}
+                    article={article}
+                    deleteArticle={deleteArticle}
+                />
+            )
+        });
+
+        return (
+            <ul>
+                Articles
+        {articleList}
+            </ul>
+        );
+    }
+};
+
+export default Articles;
